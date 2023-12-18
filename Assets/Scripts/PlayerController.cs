9234fc5e51a4c5eb84b8a8320ded4cbe59c18ef9
Celiian <90334525+Celiian@users.noreply.github.com>
2023-12-18 17:11:23 +0100

    feat(sprite) : added the samurai first movements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,31 +20,70 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private LayerMask m_GroundLayer;
 
+    [SerializeField]
+    private Animator anim;
+
 
     private bool _isGrounded = false;
 
     private bool _jump = false;
 
+    private bool _attacking = false;
 
 
     void Update()
     {
+        var horizontalInput = Input.GetAxis("Horizontal");
         _isGrounded = Physics2D.Raycast(transform.position, Vector2.down, m_PlayerHeight, m_GroundLayer);
-       
-        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
-        {
-            _jump = true;
-        }
+        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0); // Use layer 0 for the base layer.
 
+        _attacking = stateInfo.IsName("Attack 1") ? true : false;
+
+        if (!_attacking)
+        {
+            if (_isGrounded)
+            {
+                anim.SetBool("Sprint", Mathf.Abs(m_Rigidbody.velocity.x) > 1);
+
+
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    _jump = true;
+
+                }
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+
+                    anim.SetTrigger("Attack 1");
+
+                }
+
+                if (Mathf.Abs(horizontalInput) > 0.1f)
+                {
+
+                    m_Rigidbody.velocity = new Vector2(horizontalInput * m_Speed, m_Rigidbody.velocity.y);
+
+                    if (horizontalInput > 0.01f)
+                        transform.localScale = Vector3.one;
+                    else if (horizontalInput < -0.01f)
+                        transform.localScale = new Vector3(-1, 1, 1);
+
+                }
+
+            }
+            else
+            {
+                anim.SetBool("Sprint", false);
+
+            }
+        }
     }
 
 
     private void FixedUpdate()
     {
-        if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1f)
-        {
-            m_Rigidbody.velocity = new Vector2(Input.GetAxis("Horizontal") * m_Speed, m_Rigidbody.velocity.y);
-        }
+        
 
         if (_jump)
         {
