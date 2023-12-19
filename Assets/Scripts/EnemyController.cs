c3a1a5dba3249914c265fed111fa2131b6acbd38
Jerance <michael.jerance@gmail.com>
2023-12-19 11:32:28 +0100

    wip(flipping) : Enemy patrole automatic flipping in progress

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -23,32 +23,94 @@ public class EnemyController : MonoBehaviour
     [SerializeField]
     private Animator anim;
 
+    [SerializeField]
+    private float m_PatrolDistance = 1f;
 
- void Update()
+    private Vector2 m_OriginalPosition;
+    private Vector2 m_PatrolTarget;
+    private bool m_IsPatrolling = false;
+
+    private bool m_IsFacingPlayer = false;
+
+
+    void Start()
+    {
+        m_OriginalPosition = transform.position;
+        m_PatrolTarget = m_FacingLeft ? m_OriginalPosition - new Vector2(m_PatrolDistance, 0f) : m_OriginalPosition + new Vector2(m_PatrolDistance, 0f);
+    }
+    void Update()
     {
         var distanceToPlayer = Vector2.Distance(transform.position, m_Player.position);
 
         if (distanceToPlayer < m_aggroRange)
         {
+            m_IsPatrolling = false;
             var direction = (m_Player.position - transform.position).normalized;
             m_Rigidbody.velocity = new Vector2(direction.x * m_Speed, m_Rigidbody.velocity.y);
             anim.SetBool("Move", true);
 
-            if (direction.x > 0 && m_FacingLeft || direction.x < 0 && !m_FacingLeft)
+            if (!m_IsFacingPlayer)
             {
                 Flip();
             }
+            m_IsFacingPlayer = true;
         }
         else
         {
-            m_Rigidbody.velocity = new Vector2(0, m_Rigidbody.velocity.y);
-            anim.SetBool("Move", false);
+            m_IsFacingPlayer = false;
+            if (!m_IsPatrolling)
+            {
+                StartCoroutine(Patrol());
+            }
         }
     }
 
-    void Flip()
+    IEnumerator Patrol()
     {
-        m_FacingLeft = !m_FacingLeft;
-        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        m_IsPatrolling = true;
+
+        while (Vector2.Distance(transform.position, m_PatrolTarget) > 0.1f)
+        {
+            var direction = (m_PatrolTarget - (Vector2)transform.position).normalized;
+            m_Rigidbody.velocity = new Vector2(direction.x * m_Speed, m_Rigidbody.velocity.y);
+            anim.SetBool("Move", true);
+
+            yield return null;
+        }
+
+        m_Rigidbody.velocity = Vector2.zero;
+        Flip();
+
+        yield return new WaitForSeconds(0.5f);
+
+        m_PatrolTarget = m_FacingLeft ? m_OriginalPosition - new Vector2(m_PatrolDistance, 0f) : m_OriginalPosition + new Vector2(m_PatrolDistance, 0f);
+        m_IsPatrolling = false;
     }
+
+    // void Flip()
+    // {
+    //     if (m_IsFacingPlayer)
+    //     {
+    //         m_FacingLeft = !m_FacingLeft;
+    //     }
+    //     transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    // }
+
+        void Flip()
+    {
+        // if (m_IsFacingPlayer)
+        // {
+        //     m_FacingLeft = !m_FacingLeft;
+        // }
+        if(m_FacingLeft)
+        {
+            transform.localScale = Vector3.one;
+             m_FacingLeft = false;
+
+        }
+      else {
+            m_FacingLeft = true;
+            transform.localScale = new Vector3(-1, 1, 1);
+       }
+    }  
 }
