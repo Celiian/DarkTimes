9b4a904c74fd2dd3016edf53fd134b3ab92f48c2
Celiian <90334525+Celiian@users.noreply.github.com>
2023-12-19 01:33:25 +0100

    feat(attack) : added rayCaster for basic attacks)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,9 @@ public class PlayerController : MonoBehaviour
     private float m_JumpForce;
 
     [SerializeField]
+    private float m_AttackRange;
+
+    [SerializeField]
     private Rigidbody2D m_Rigidbody;
 
     [SerializeField]
@@ -21,15 +24,24 @@ public class PlayerController : MonoBehaviour
     private LayerMask m_GroundLayer;
 
     [SerializeField]
+    private LayerMask m_EnemyLayer;
+
+    [SerializeField]
     private Animator animSpear;
 
     [SerializeField]
     private Animator animSword;
 
+    [SerializeField]
+    private bool m_Double_jump;
+
+
 
     private bool _isGrounded = false;
 
     private bool _jump = false;
+
+    private bool _jumped_twice = false;
 
     private bool _attacking = false;
 
@@ -73,9 +85,9 @@ public class PlayerController : MonoBehaviour
         AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
 
 
+
         _isGrounded = Physics2D.Raycast(transform.position, Vector2.down, m_PlayerHeight, m_GroundLayer);
         anim.SetBool(GroundMode, Physics2D.Raycast(transform.position, Vector2.down, (float)(m_PlayerHeight * 1.6), m_GroundLayer));
-
 
         _attacking = stateInfo.IsName(attackMode) ? true : false;
        
@@ -85,74 +97,77 @@ public class PlayerController : MonoBehaviour
             {
                 anim.SetBool(sprintMode, Mathf.Abs(m_Rigidbody.velocity.x) > 1);
 
-
-                if (Input.GetKeyDown(KeyCode.Space))
-                {
-                    _jump = true;
-                    anim.SetTrigger(JumpMode);
-
-                }
-
                 if (Input.GetKeyDown(KeyCode.L))
                 {
 
                     _spearMode = !_spearMode;
-                    _sprint = false;
                 }
 
                 if (Input.GetKeyDown(KeyCode.K))
                 {
 
                     anim.SetTrigger(attackMode);
-                    _sprint = false;
                 }
 
-                if (Input.GetKeyDown(KeyCode.LeftShift))
+                if (Input.GetKey(KeyCode.LeftShift))
                 {
+
                     _sprint = true;
-                   
                 }
-
-                if (Input.GetKeyUp(KeyCode.LeftShift))
+                else
                 {
                     _sprint = false;
-
                 }
-
-                if (Mathf.Abs(horizontalInput) > 0.1f)
-                {
-
-                    var speed = (float)(m_Speed * (_sprint ? 1.7 : 1));
-
-                    m_Rigidbody.velocity = new Vector2(horizontalInput * speed, m_Rigidbody.velocity.y);
-
-                    if (horizontalInput > 0.01f)
-                        transform.localScale = Vector3.one;
-                    else if (horizontalInput < -0.01f)
-                        transform.localScale = new Vector3(-1, 1, 1);
-
-                }
-
+                                              
             }
-            else
+
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                if (Mathf.Abs(horizontalInput) > 0.1f)
+                if (_isGrounded)
                 {
-
-                    var speed = (float)(m_Speed * (_sprint ? 1.7 : 1));
-
-                    m_Rigidbody.velocity = new Vector2(horizontalInput * speed, m_Rigidbody.velocity.y);
-
-                    if (horizontalInput > 0.01f)
-                        transform.localScale = Vector3.one;
-                    else if (horizontalInput < -0.01f)
-                        transform.localScale = new Vector3(-1, 1, 1);
-
+                    _jump = true;
+                    _jumped_twice = false;
+                    anim.SetTrigger(JumpMode);
                 }
+                else if (m_Double_jump)
+                {
+                    if (!_jumped_twice)
+                    {
+                        _jump = true;
+                        anim.SetTrigger(JumpMode);
+                        _jumped_twice = true;
+                    }
+                }
+            }
 
-                // anim.SetBool(sprintMode, false);
+         
+            if (Mathf.Abs(horizontalInput) > 0.1f)
+            {
+
+                var speed = (float)(m_Speed * (_sprint ? 1.7 : 1));
+
+                m_Rigidbody.velocity = new Vector2(horizontalInput * speed, m_Rigidbody.velocity.y);
+
+                if (horizontalInput > 0.01f)
+                    transform.localScale = Vector3.one;
+                else if (horizontalInput < -0.01f)
+                    transform.localScale = new Vector3(-1, 1, 1);
 
             }
+        }
+        else
+        {
+            Vector2 rayDirection = horizontalInput > 0 ? Vector2.right : Vector2.right;
+
+            var AttackRange = _spearMode ? (float)( m_AttackRange * 1.6 ):( m_AttackRange);
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, rayDirection, AttackRange, m_EnemyLayer);
+
+            if (hit.collider != null)
+            {
+                Debug.Log("Touching enemy");
+            }
+
         }
     }
 
@@ -161,8 +176,11 @@ public class PlayerController : MonoBehaviour
     {
         if (_jump)
         {
+
+            var force = _jumped_twice ? (float)(m_JumpForce * 1.1) : m_JumpForce;
+
             _jump = false;
-            m_Rigidbody.AddForce(new Vector2(0, m_JumpForce), ForceMode2D.Impulse);
+            m_Rigidbody.AddForce(new Vector2(0, force), ForceMode2D.Impulse);
         }
     }
 
