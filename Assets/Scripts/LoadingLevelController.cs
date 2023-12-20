59641c27c2021c734de849f3f7890e4406a4c29b
Couanon Thibault <103991748+TitiBond@users.noreply.github.com>
2023-12-20 12:39:52 +0100

    feat(change scene): adding end level with door to change scene

--- Assets/Scripts/LoadingLevelController.cs
+++ Assets/Scripts/LoadingLevelController.cs
@@ -1,18 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadingLevelController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private BoxCollider2D doorTrigger;
+    [SerializeField] 
+    private LayerMask m_PlayerLayer;
+    public string LevelToLoad;
+    
+    
+
+    void LoadLevel()
     {
+
+        Collider2D[] colliders = new Collider2D[100];
+
+        ContactFilter2D contactFilter = new ContactFilter2D();
+        contactFilter.SetLayerMask(m_PlayerLayer);
+
+        int numColliders = doorTrigger.OverlapCollider(contactFilter, colliders);
+
+        for (int i = 0; i < numColliders; i++)
+        {
+            Collider2D collider = colliders[i];
+
+            Debug.Log(collider.tag);
+            if (collider.CompareTag("Joueur"))
+            {
+                SceneManager.LoadScene(LevelToLoad);
+                break;
+            }
+        }
+
+
+
+
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        LoadLevel();
+
     }
 }
